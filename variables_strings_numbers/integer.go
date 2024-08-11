e155f912755c0403361de64ff1f6d009ed8d891b
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-08-11 18:41:32 +0600

    feat: Add integer variable examples

---
+++ variables_strings_numbers/integer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func integer() {
+
+	// int
+	var ageOne int = 10
+	var ageTwo = 20
+	ageThree := 30
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bit
+	var bit8_1 int8 = 127
+	var bit8_2 int16 = -127
+	var uint8_1 uint8 = 255
+
+	fmt.Println(bit8_1, bit8_2, uint8_1)
+
+}
