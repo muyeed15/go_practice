6bffde89861bf9657a9f964fa2e328cd8f703b04
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-08-11 18:41:45 +0600

    Add format_string package with format function

---
+++ format_string/format.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func format() {
+	var name = "Muyeed"
+	var age = 23
+
+	// print
+	fmt.Printf("My name is %v and I'm %v years old.", name, age)
+
+	// type
+	fmt.Printf("\n%T", age)
+
+	// float
+	fmt.Printf("\n%f", 12.44565)
+	fmt.Printf("\n%0.1f", 12.44565)
+	fmt.Printf("\n%0.2f", 12.44565)
+	fmt.Printf("\n%0.3f", 12.44565)
+
+	// sprintf
+	var str = fmt.Sprintf("My name is %v and I'm %v years old.", name, age)
+	fmt.Println("\nSaved String: ", str)
+
+}
