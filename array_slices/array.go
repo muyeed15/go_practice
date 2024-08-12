96705e48bd0fcef3f0f6caef3412419a7b3ef276
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-08-12 12:51:21 +0600

    Add array_slices package with array function

---
+++ array_slices/array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func array() {
+
+	// type with capacity
+	var arr1 [3]int = [3]int{20, 25, 30}
+
+	// without type
+	var arr2 = [4]string{"Muyeed", "Dipra", "Alam"}
+
+	fmt.Println(arr1, len(arr2))
+
+}
