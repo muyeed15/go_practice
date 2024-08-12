a57fc3839cda1bdabc9f1d5f9929789874a6ac58
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-08-12 12:51:27 +0600

    feat: Add slices example to array_slices package

---
+++ array_slices/slices.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// slices without capacity
+	var slc1 = []int{10, 30, 40}
+
+	fmt.Println(slc1)
+
+	// replace
+	slc1[1] = 20
+
+	// append
+	slc1 = append(slc1, 50)
+
+	fmt.Println(slc1)
+
+	// slice range
+	range1 := slc1[1:3]
+	range2 := slc1[2:]
+	range3 := slc1[:3]
+	range4 := append(range2, 80)
+	fmt.Println(range1, range2, range3, range4)
+
+}
