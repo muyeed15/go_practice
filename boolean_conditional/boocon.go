4d1fdac51e25db55d764fda9186ad88225fcae3a
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-08-12 13:36:22 +0600

    Add boolean conditional examples

---
+++ boolean_conditional/boocon.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func boocon() {
+	age := 45
+
+	// direct bool
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	// condtion
+	if age < 30 {
+		fmt.Println("true")
+	} else if age == 45 {
+		fmt.Println("yuppies!")
+	} else {
+		fmt.Println("false")
+	}
+
+	// continue and break
+	names := []string{"Muyeed", "Dipra", "Alam", "Romeo", "Diya", "Shrabon"}
+
+	for index, value := range names {
+		if index == 2 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+
+		if index == 4 {
+			fmt.Println("Breaking at pos", index)
+			break
+		}
+
+		fmt.Printf("The value of pos %v is %v\n", index, value)
+
+	}
+
+}
