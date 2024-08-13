22dfabcd8122bf3beac36de5c652a058a538caa5
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-08-13 16:06:59 +0600

    Update main.go to include structsreciever, maps, and pointers packages

--- main.go
+++ main.go
@@ -1,34 +1,44 @@
 package main
 
-import (
-	"go_practice/arrayslices"
-	"go_practice/booleanconditional"
-	"go_practice/format"
-	"go_practice/functions"
-	"go_practice/helloworld"
-	lopps "go_practice/loops"
-	"go_practice/maps"
-	"go_practice/packages"
-	"go_practice/pointers"
-	"go_practice/scopes"
-	"go_practice/structsreciever"
-	"go_practice/variablesstringsnumbers"
-)
+// import (
+// 	"go_practice/arrayslices"
+// 	"go_practice/booleanconditional"
+// 	"go_practice/forloops"
+// 	"go_practice/format"
+// 	"go_practice/functions"
+// 	"go_practice/helloworld"
+// 	"go_practice/interfaces"
+// 	"go_practice/maps"
+// 	"go_practice/packages"
+// 	"go_practice/pointers"
+// 	"go_practice/savefile"
+// 	"go_practice/scopes"
+// 	"go_practice/stringconvert"
+// 	"go_practice/structsreciever"
+// 	"go_practice/switchcase"
+// 	"go_practice/userinput"
+// 	"go_practice/variablesstringsnumbers"
+// )
 
-func main() {
-	format.Run()
-	booleanconditional.Run()
-	arrayslices.RunArray()
-	arrayslices.RunSlices()
-	functions.Run()
-	helloworld.Run()
-	lopps.Run()
-	packages.Run()
-	scopes.Run()
-	variablesstringsnumbers.RunFloat()
-	variablesstringsnumbers.RunInt()
-	variablesstringsnumbers.RunVar()
-	maps.Run()
-	pointers.Run()
-	structsreciever.Run()
-}
+// func main() {
+// 	format.Run()
+// 	booleanconditional.Run()
+// 	arrayslices.RunArray()
+// 	arrayslices.RunSlices()
+// 	functions.Run()
+// 	helloworld.Run()
+// 	forloops.Run()
+// 	packages.Run()
+// 	scopes.Run()
+// 	variablesstringsnumbers.RunFloat()
+// 	variablesstringsnumbers.RunInt()
+// 	variablesstringsnumbers.RunVar()
+// 	maps.Run()
+// 	pointers.Run()
+// 	structsreciever.Run()
+// 	switchcase.Run("a")
+// 	stringconvert.Run()
+// 	savefile.Run()
+// 	interfaces.Run()
+// 	userinput.Run()
+// }
